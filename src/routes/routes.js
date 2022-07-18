777da5c2927b89a99aebe859a6ddde0cec91c66f
ankita9809 <k.ankita037@gmail.com>
2022-07-18 14:10:52 +0530

    Ankita

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const urlController = require('../controllers/urlController')
 
-// ------------------------ POST and GET Url -----------------------------
+// ------------------------ Create URL -----------------------------
 router.post('/url/shorten', urlController.createUrl)
+
+// ------------------------ GET /:urlCode -----------------------------
+router.get('/:urlCode', urlController.getUrl)
 
 module.exports = router
 
